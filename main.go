a3ee74a3f5e4d1d616ab3b6a25aa4f88765a60a3
aivruu <benjaalejandro206@gmail.com>
2024-10-31 15:39:10 -0300

    feat(main): Implement files-download functionality for command-line

--- main.go
+++ main.go
@@ -24,7 +24,9 @@ package main
 import (
 	"fmt"
 	"os"
+	"strconv"
 	"strings"
+	"viewer/main/download"
 	"viewer/main/http"
 	"viewer/main/repository"
 )
@@ -33,12 +35,10 @@ func main() {
 	argsAmount := len(os.Args)
 	if argsAmount < 2 {
 		fmt.Println("Missing arguments.")
-		os.Exit(1)
 		return
 	}
-	if argsAmount < 3 || argsAmount > 4 {
+	if argsAmount < 3 || argsAmount > 6 {
 		fmt.Println("Invalid arguments amount, four for release-info, five for repository.")
-		os.Exit(2)
 		return
 	}
 	if argsAmount == 4 {
@@ -46,47 +46,81 @@ func main() {
 		model := http.Request(releaseRequest)
 		if model == nil {
 			fmt.Println("Failed to request the release for this repository.")
-			os.Exit(3)
 			return
 		}
 		printReleaseInformation(model)
+		return
+	} else if argsAmount == 6 && strings.Contains(os.Args[3], ".") {
+		releaseRequest := repository.NewReleaseRequest(ForRelease(os.Args[1], os.Args[2], os.Args[3]))
+		model := http.Request(releaseRequest)
+		if model == nil {
+			fmt.Println("Failed to request the release for asset download.")
+			return
+		}
+		if os.Args[4] == "*" {
+			fmt.Println("Downloading all release's assets...")
+			downloadAllAssets(os.Args[5], model)
+		} else {
+			index, err := strconv.Atoi(os.Args[4])
+			if err != nil {
+				fmt.Println("Not valid index-value for asset download.")
+				return
+			}
+			downloadAsset(os.Args[5], model, index-1)
+		}
 		return
 	}
 	repositoryRequest := repository.NewRepositoryRequest(ForRepository(os.Args[1], os.Args[2]))
 	model := http.Request(repositoryRequest)
 	if model == nil {
 		fmt.Println("Failed to request the repository.")
-		os.Exit(3)
 		return
 	}
 	printRepositoryInformation(model)
 }
 
+func downloadAsset(directory string, model *repository.GithubReleaseModel, index int) {
+	read := model.Download(directory, index)
+	fmt.Println("Downloading asset...")
+	if read == download.InvalidAssetDefaultSize || read == download.UnknownAssetDefaultSize {
+		fmt.Printf("This asset couldn't be downloaded, may be due to an out of range value, index '%d' assets-amount '%d'", index, len(model.Assets))
+		return
+	}
+	fmt.Printf("Downloaded asset with name '%s' and '%d' read bytes. ", model.Assets[index].Name, read)
+}
+
+func downloadAllAssets(directory string, model *repository.GithubReleaseModel) {
+	for index := range model.Assets {
+		downloadAsset(directory, model, index)
+	}
+}
+
 func printReleaseInformation(model *repository.GithubReleaseModel) {
 	fmt.Println("Showing information for repository's release: ", model.TagName)
-	fmt.Println("Title -> ", model.Name)
-	fmt.Println("Tag -> ", model.TagName)
-	fmt.Println("Author -> ", model.Author)
-	fmt.Println("Identifier -> ", model.UniqueId)
+	fmt.Println("Title ->", model.Name)
+	fmt.Println("Tag ->", model.TagName)
+	fmt.Println("Author ->", model.Author)
+	fmt.Println("Identifier ->", model.UniqueId)
 	fmt.Println("Assets:")
-	for _, asset := range model.Assets {
-		fmt.Println("  Name -> ", asset.Name)
-		fmt.Println("  URL -> ", asset.Url)
+	for index, asset := range model.Assets {
+		fmt.Println("  Index ->", index+1)
+		fmt.Println("  Name ->", asset.Name)
+		fmt.Println("  URL ->", asset.Url)
 	}
 }
 
 func printRepositoryInformation(model *repository.GithubRepositoryModel) {
-	fmt.Println("Showing information for repository: ", model.Name)
+	fmt.Println("Showing information for repository:", model.Name)
 	fmt.Println()
-	fmt.Println("Owner -> ", model.Owner.Login)
-	fmt.Println("Description -> ", model.Description)
-	fmt.Println("Topics -> ", strings.Join(model.Topics, ", "))
-	fmt.Println("Stars -> ", model.Stars)
-	fmt.Println("Forks -> ", model.Forks)
-	fmt.Println("Forks Allowed -> ", repository.FormatBooleanValue(model.CanFork))
-	fmt.Println("Language -> ", model.Language)
-	fmt.Println("License -> ", model.License.Name)
-	fmt.Println("Public -> ", repository.FormatBooleanValue(!model.Private))
-	fmt.Println("Archived -> ", repository.FormatBooleanValue(model.Archived))
-	fmt.Println("Disabled -> ", repository.FormatBooleanValue(model.Disabled))
+	fmt.Println("Owner ->", model.Owner.Login)
+	fmt.Println("Description ->", model.Description)
+	fmt.Println("Topics ->", strings.Join(model.Topics, ", "))
+	fmt.Println("Stars ->", model.Stars)
+	fmt.Println("Forks ->", model.Forks)
+	fmt.Println("Forks Allowed ->", repository.FormatBooleanValue(model.CanFork))
+	fmt.Println("Language ->", model.Language)
+	fmt.Println("License ->", model.License.Name)
+	fmt.Println("Public ->", repository.FormatBooleanValue(!model.Private))
+	fmt.Println("Archived ->", repository.FormatBooleanValue(model.Archived))
+	fmt.Println("Disabled ->", repository.FormatBooleanValue(model.Disabled))
 }

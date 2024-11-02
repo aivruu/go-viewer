0c9db47ec7760ec08de9503b806bf370282672fb
aivruu <benjaalejandro206@gmail.com>
2024-11-02 18:08:57 -0300

    chore(response_helper): Remove duplicated code, simplify logic for Response function, and use built-in default Client instance

--- utils/response_helper.go
+++ utils/response_helper.go
@@ -29,11 +29,19 @@ import (
 	vhttp "viewer/main/http"
 )
 
-// AsyncResponseWith This function makes a request to the given url using the given http.Client, and returns an async.Future,
-// this object's function may return a http.ResponseModel, or nil depending on operation success.
-func asyncResponseWith(client *http.Client, url string) async.Future[vhttp.ResponseModel] {
+// AsyncResponseWith This function makes a request to the given url using the given http.Client, if a Client instance is not
+// provided, will use the default http.DefaultClient for the request. It will return an async.Future, this object's function
+// may return a http.ResponseModel, or nil depending on operation success.
+func asyncResponse(client *http.Client, url string) async.Future[vhttp.ResponseModel] {
 	return async.NewFuture(func() *vhttp.ResponseModel {
-		resp, err := client.Get(url)
+		var resp *http.Response
+		var err error
+		// Verify if a specific [Client] instance was provided.
+		if client == nil {
+			resp, err = http.Get(url) // Use default client instance.
+		} else {
+			resp, err = client.Get(url)
+		}
 		if err != nil {
 			fmt.Println("Error during request: ", err)
 			return nil
@@ -55,49 +63,19 @@ func asyncResponseWith(client *http.Client, url string) async.Future[vhttp.Respo
 	})
 }
 
-// Response This function realizes a request and provides an async response of async.Future type, this function checks if the
-// provided http.Client is nil, if it is, it will call to the asyncResponse function, otherwise, it will use asyncResponseWith
+// Response This function calls internally to the asyncResponse and when is available, it will return the http.ResponseModel
+// provided by that function.
 func Response(client *http.Client, url string) *vhttp.ResponseModel {
-	var response async.Future[vhttp.ResponseModel]
-	if client == nil {
-		response = asyncResponse(url)
-	} else {
-		response = asyncResponseWith(client, url)
-	}
-	return response.Get()
-}
-
-// AsyncResponse This function makes a request to the given url using a default client and returns an async.Future,
-// this object's function may return a http.ResponseModel, or nil depending on operation success.
-func asyncResponse(url string) async.Future[vhttp.ResponseModel] {
-	return async.NewFuture(func() *vhttp.ResponseModel {
-		resp, err := vhttp.DefaultClient.Get(url)
-		if err != nil {
-			fmt.Println("Error during request: ", err)
-			return nil
-		}
-		body := resp.Body
-		read, err := io.ReadAll(body)
-		if err != nil {
-			fmt.Println("Error during reading: ", err)
-			return nil
-		}
-		// Close body after reading.
-		defer func(Body *io.ReadCloser) {
-			err := (*Body).Close()
-			if err != nil {
-				fmt.Println("Error during Body closing: ", err)
-			}
-		}(&body)
-		return vhttp.NewResponseModel(string(read), resp.StatusCode, &body)
-	})
+	f := asyncResponse(client, url)
+	// Return [ResponseModel] object when available.
+	return f.Get()
 }
 
 // OriginalResponse This function makes an async request to the given url, and returns the built-in http.Response object,
 // and not a http.ResponseModel.
 func OriginalResponse(url string) async.Future[http.Response] {
 	return async.NewFuture(func() *http.Response {
-		resp, err := vhttp.DefaultClient.Get(url)
+		resp, err := http.Get(url)
 		if err != nil {
 			fmt.Println("Error during request: ", err)
 			return nil
